a92c47b3c7b0524da4fa687c5145f4bb68095c8f
Brian Schmeling <brianschmeling2@gmail.com>
2022-06-21 12:55:13 -0400

    Complete backend and create react app for frontend

--- backend/controllers/addressBook.js
+++ backend/controllers/addressBook.js
@@ -17,4 +17,29 @@ router.get("/:firstName/:lastName", (req, res) => {
   });
 });
 
+router.post("/new", (req, res) => {
+  AddressBook.create(req.body).then((address) => {
+    res.json(address);
+  });
+});
+
+router.put("/:firstName/:lastName", (req, res) => {
+  AddressBook.findOneAndUpdate(
+    { firstName: req.params.firstName, lastName: req.params.lastName },
+    req.body,
+    { new: true }
+  ).then((address) => {
+    res.json(address);
+  });
+});
+
+router.delete("/:firstName/:lastName", (req, res) => {
+  AddressBook.findOneAndDelete({
+    firstName: req.params.firstName,
+    lastName: req.params.lastName,
+  }).then((address) => {
+    res.json(address);
+  });
+});
+
 module.exports = router;
